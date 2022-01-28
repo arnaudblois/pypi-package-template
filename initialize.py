731e5aeb9a67f8aae7a172191e40bb881135a309
Arnaud Blois <a.blois@ucl.ac.uk>
2022-01-28 00:38:33 +0000

    fix: git_url is correctly infered or the user is prompted

--- initialize.py
+++ initialize.py
@@ -1,8 +1,11 @@
 """Initialize the context for templating."""
 
 from datetime import date
+
 import subprocess
-from pytemplator.utils import Question as Q, Context
+
+from pytemplator.exceptions import InvalidInputError
+from pytemplator.utils import cd, Question as Q, Context
 
 def generate_context(no_input, *args, **kwargs):
     """Generate context."""
@@ -10,20 +13,34 @@ def generate_context(no_input, *args, **kwargs):
     context = Context()
     try:
         default_author_name = subprocess.run(["git", "config", "user.name"], capture_output=True, text=True).stdout.strip()
-    except FileNotFoundError:
+    except subprocess.CalledProcessError:
         default_author_name = None
     try:
         default_author_email = subprocess.run(["git", "config", "user.email"], capture_output=True, text=True).stdout.strip()
-    except FileNotFoundError:
+    except subprocess.CalledProcessError:
         default_author_email = None
     try:
         default_git_url = subprocess.run(["git", "config", "remote.origin.url"], capture_output=True, text=True).stdout.strip()
         default_git_url = default_git_url.replace("git@github.com:", "https://github.com/")
-    except FileNotFoundError:
-        default_author_email = None
+    except subprocess.CalledProcessError:
+        default_git_url = None
+
+    if not default_git_url:
+        git_question = Q(
+            "git_url",
+            ask="URL of the remote Git repo where this project has been initialised: "
+        )
+        git_question.resolve(no_input)
+        try:
+            subprocess.run(["git", "clone", git_question.answer], check=True)
+        except subprocess.CalledProcessError as error:
+            raise InvalidInputError from error
+    else:
+        git_question = Q("git_url", ask=False, default=default_git_url)
 
     context.questions = [
-        Q("pypi_name", ask="Name of the package on Pypi"),
+        git_question,
+        Q("pypi_name", ask="Name of the package on Pypi: "),
         Q("module_name", ask=False, default=lambda: context["pypi_name"].replace("-","_").lower()),
         Q("title", default=lambda: context["pypi_name"].replace("-"," ").title()),
         Q("description"),
